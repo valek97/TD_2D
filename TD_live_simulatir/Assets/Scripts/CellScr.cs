f60c14a901e1f6ce99a55d9004bff92f7814f1b1
valek97 <kritenko97@gmail.com>
2020-04-22 00:19:22 +0300

    Урок 1 без комментариев

---
+++ TD_live_simulatir/Assets/Scripts/CellScr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellScr : MonoBehaviour
+{
+    public int state, id;
+
+    public Color normCol, pathCol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetState(int i)
+    {
+        state = i;
+        if (i == 0)
+            GetComponent<Image>().color = normCol;
+        if (i==1)
+            GetComponent<Image>().color = pathCol;
+
+    }
+}
---
+++ TD_live_simulatir/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int cellCount;
+
+    int[] pathID = {23,24,25,47,69,70,71,93,94,95,96,
+                    97,119,141,142,164,165,166,167,
+                    189,211,212,213,214,215,193,194,
+                    195,196,197,198};
+
+    List<CellScr> AllCells = new List<CellScr>();
+    public GameObject cellPref;
+    public Transform cellGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateCells();
+        CreatePath();
+
+    }
+
+   
+
+    /// <summary>
+    /// Метод создания клеток.
+    /// </summary>
+    private void CreateCells()
+    {
+        for (int i = 0; i<cellCount;i++)
+        {
+            
+            GameObject tmpCell = Instantiate(cellPref);
+            tmpCell.SetActive(true);
+            tmpCell.transform.SetParent(cellGroup, false);
+            tmpCell.GetComponent<CellScr>().id = i + 1;
+            tmpCell.GetComponent<CellScr>().SetState(0);
+            AllCells.Add(tmpCell.GetComponent<CellScr>());
+            
+        }
+    }
+    void CreatePath()
+    {
+        for (int i = 0; i < pathID.Length; i++)
+        {
+            AllCells[pathID[i] - 1].SetState(1);
+        }
+    }
+
+}
